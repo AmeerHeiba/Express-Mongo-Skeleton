668f895d73c4212f9202c64da0e9ea4c5549a667
AmeerHeiba <31767185+AmeerHeiba@users.noreply.github.com>
2024-08-25 17:45:23 +0300

    new branch

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Routes
 
